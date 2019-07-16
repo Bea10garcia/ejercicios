317c8608f4c35759b94941aabbd0eb48f6794baa
Cpl <carlospdl@egeontechnology.com>
2019-07-16 11:55:32 +0200

    Resuelto con comprobación de 8 dígitos

--- Ejercicio3/ejercicio3-1.js
+++ Ejercicio3/ejercicio3-1.js
@@ -2,11 +2,22 @@ function CalculaLetradni(dni){
 	var letras = "TRWAGMYFPDXBNJZSQVHLCKE";
 	var resto;
 	var letra;
-	resto = dni % 23;
-	letra=letras.substr(resto,1);
-	console.log("dni=" +dni);
-	console.log("letra= "+letra);
-	document.getElementById("nrodni").innerHTML = dni;
-	document.getElementById("letra").innerHTML = letra;
+	// dni es un objeto del DOM (inputElement), por lo que para acceder al 
+	// valor, utilizamos la propiedad value
+
+	//	Si el dni no es un nro, es que tiene letras y no puede procesarse
+	if(isNaN(dni.value)){
+		alert("El nro. de dni debe contener sólo cifras.");
+		// si ponemos la linea siguiente, se presenta el mensaje y se termina.
+		// Otra opción es utilizar else (la que dejamos) 
+		//return;
+	} else {
+		resto = dni.value % 23;
+		letra=letras.substr(resto,1);
+		console.log("dni=" +dni.value);
+		console.log("letra= "+letra);
+		document.getElementById("nrodni").innerHTML = dni.value;
+		document.getElementById("letra").innerHTML = letra;
+	}
 	return;
 }
