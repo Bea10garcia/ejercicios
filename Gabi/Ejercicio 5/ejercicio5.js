fae54b6e732a71c89f8e94f846b49225d3d8fe3c
gabrielgarci <52569853+gabrielgarci@users.noreply.github.com>
2019-08-12 18:43:57 +0200

    Añadido ejercicio 5

---
+++ Gabi/Ejercicio 5/ejercicio5.js
@@ -0,0 +1,28 @@
+// Funcion para determinar coordenadas del ratón respecto al navegador y toda la pantalla
+function coordenadas(event) {
+    var xnav;
+    var ynav;
+    var xpan;
+    var ypan;
+    xnav=event.pageX;
+    ynav=event.pageY;
+    xpan=event.screenX;
+    ypan=event.screenY;
+    document.getElementById("xnav").innerHTML = xnav;
+    document.getElementById("ynav").innerHTML = ynav;
+    document.getElementById("xpan").innerHTML = xpan;
+    document.getElementById("ypan").innerHTML = ypan;
+    document.getElementById("raton").style.backgroundColor = "white";
+}
+
+// Cambiar color al pulsar tecla
+function colorRaton(){
+    document.getElementById("raton").style.backgroundColor = "#FFFFCC"
+}
+
+// Representar caractér pulsado y su código ascii
+window.addEventListener("keypress", function(tecla){
+    this.document.getElementById("letra").innerHTML = tecla.key;
+    this.document.getElementById("codigo").innerHTML = tecla.key.charCodeAt(0);
+    document.getElementById("teclado").style.backgroundColor = "#CCE6FF"
+});
