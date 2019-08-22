f5a03a7eab726fdfadefb1a99f3c64304daa0b9b
Lorena <Lneirax@gmail.com>
2019-08-22 13:03:51 +0200

    Ejercicio par o impar

---
+++ Lorena/01-cadenasynumeros/01-cadenas_y_numeros.js
@@ -0,0 +1,20 @@
+/*Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar.
+Mostrar por pantalla el resultado devuelto por la función.*/
+
+function parImpar(numero){
+	numero = parseInt(numero);
+	//console.log(typeof(numero));
+
+	if(numero<0){
+		alert("Ingresa un número entero");
+		
+	}else if(numero%2 == 0){		
+		document.getElementById("resultado").innerHTML ="El número "+numero+" es par";
+		
+	}else{
+		document.getElementById("resultado").innerHTML ="El número "+numero+" es impar";
+		
+	}
+}
+
+
