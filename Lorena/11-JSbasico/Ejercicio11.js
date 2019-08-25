b0a4d0b01861f72d9f2ed83ccf7e2b84e7090f60
Lorena <Lneirax@gmail.com>
2019-08-25 04:50:53 +0200

    Comparador de números

---
+++ Lorena/11-JSbasico/Ejercicio11.js
@@ -0,0 +1,13 @@
+"use strict"
+
+function compararNumeros(num1,num2){
+	
+	if(num1>num2){
+		alert("El número "+num1+" es mayor que el número "+num2);
+	}else if(num1<num2){
+		alert("El número "+num1+" es menor que el número "+num2);
+	}else if(num1==num2){
+		alert("Los números son iguales");
+	}
+}
+
