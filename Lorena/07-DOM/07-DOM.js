acebb344f8772cb9eed6eaca98c00c513b6ddde4
Lorena <Lneirax@gmail.com>
2019-08-26 12:58:23 +0200

    Ejercicio ocultar/mostrar

--- Lorena/07-DOM/07-DOM.js
+++ Lorena/07-DOM/07-DOM.js
@@ -1,12 +1,14 @@
 "use strict"
 
-function ocultarMostrar(seccion){
+function ocultarMostrar(seccion,referenciaBoton){
 	seccion.style.display = (seccion.style.display == 'none') ? 'block' : 'none';
 		
 			if(seccion.style.display == 'none'){
-				document.getElementById("botonOcultarMostrar").innerHTML = "Mostrar contenidos";
-			}else if(seccion.style.display == 'block'){
-				document.getElementById("botonOcultarMostrar").innerHTML = "Ocultar contenidos";
+				//document.getElementById("botonOcultarMostrar").innerHTML = "Mostrar contenidos";
+				referenciaBoton.innerHTML = "Mostrar contenido";
+			}else{
+				//document.getElementById("botonOcultarMostrar").innerHTML = "Ocultar contenidos";
+				referenciaBoton.innerHTML = "Ocultar contenidos";
 			}
 }
 	
