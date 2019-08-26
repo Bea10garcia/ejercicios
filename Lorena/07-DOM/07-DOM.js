72a3671bc0d86ca82aa318a7f2dc7e1a82d32cff
LNeirax <52679958+LNeirax@users.noreply.github.com>
2019-08-26 13:00:30 +0200

    Update 07-DOM.js

--- Lorena/07-DOM/07-DOM.js
+++ Lorena/07-DOM/07-DOM.js
@@ -4,11 +4,11 @@ function ocultarMostrar(seccion,referenciaBoton){
 	seccion.style.display = (seccion.style.display == 'none') ? 'block' : 'none';
 		
 			if(seccion.style.display == 'none'){
-				//document.getElementById("botonOcultarMostrar").innerHTML = "Mostrar contenidos";
 				referenciaBoton.innerHTML = "Mostrar contenido";
+				
 			}else{
-				//document.getElementById("botonOcultarMostrar").innerHTML = "Ocultar contenidos";
 				referenciaBoton.innerHTML = "Ocultar contenidos";
+				
 			}
 }
 	
